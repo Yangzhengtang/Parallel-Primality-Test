df9f00be0229a4f197f7144a24afeb4c3a0c5285
Zhengtang <yangzhengtang@utexas.edu>
2021-11-11 23:56:05 -0600

    Refine benchmarking codes.

--- rm_parallel.h
+++ rm_parallel.h
@@ -11,19 +11,18 @@
 #define PTHREAD 8
 
 static pthread_t running_threads[PTHREAD];
-static unsigned int running_thread_num = 0;
 
 struct single_thread_RM_args
 {
-	unsigned int test_num;
+    unsigned int test_num;
     unsigned int k;
-    unsigned int d; 
+    unsigned int d;
     unsigned int s;
-    int*         ret;
+    int *ret;
 };
 
-
-int rabin_miller_shitty_parallel(unsigned int test_num, unsigned int k){
+int rabin_miller_shitty_parallel(unsigned int test_num, unsigned int k)
+{
     //  First do some input validation
     if (test_num < 2)
         return 0;
@@ -41,11 +40,11 @@ int rabin_miller_shitty_parallel(unsigned int test_num, unsigned int k){
         s += 1;
     }
 
-    int *isComposite = (int *)(malloc(sizeof(int) * (k+1)));
+    int *isComposite = (int *)(malloc(sizeof(int) * (k + 1)));
 
-    #pragma omp parallel for num_threads(PTHREAD)
+#pragma omp parallel for num_threads(PTHREAD)
     for (unsigned int i = 0; i < k; i++)
-    { //  [Par-Opt]   This for loop can be performed in parallel
+    {                                             //  [Par-Opt]   This for loop can be performed in parallel
         unsigned int a = getRandom(test_num - 1); //  pick a randomly in the range [2, n − 1]
         unsigned int x = expoMod(a, d, test_num);
         int early_terminate = 0;
@@ -84,35 +83,41 @@ int rabin_miller_shitty_parallel(unsigned int test_num, unsigned int k){
     return ret;
 }
 
-void single_RM_thread(struct single_thread_RM_args* args){
+void single_RM_thread(struct single_thread_RM_args *args)
+{
     unsigned int test_num = args->test_num;
     unsigned int k = args->k;
     unsigned int d = args->d;
     unsigned int s = args->s;
-    int*    ret = args->ret;
+    int *ret = args->ret;
 
-    for(unsigned int i=0; i<k; i++){    //  [Par-Opt]   This for loop can be performed in parallel
-        unsigned int a = getRandom(test_num - 1);   //  pick a randomly in the range [2, n − 1]
+    for (unsigned int i = 0; i < k; i++)
+    {                                             //  [Par-Opt]   This for loop can be performed in parallel
+        unsigned int a = getRandom(test_num - 1); //  pick a randomly in the range [2, n − 1]
         unsigned int x = expoMod(a, d, test_num);
         int early_terminate = 0;
-        if((x == 1) || (x == (test_num-1)))   //  if x = 1 or x = n − 1 then do next LOOP
+        if ((x == 1) || (x == (test_num - 1))) //  if x = 1 or x = n − 1 then do next LOOP
             continue;
-        
-        for(unsigned int r=1; r<s; r++){
+
+        for (unsigned int r = 1; r < s; r++)
+        {
             x = expoMod(x, 2, test_num);
-            if(x == 1){
+            if (x == 1)
+            {
                 *ret = 0;
                 return;
-            }  
-            if(x == test_num-1){
+            }
+            if (x == test_num - 1)
+            {
                 early_terminate = 1;
                 break;
             }
         }
 
-        if(!early_terminate){
+        if (!early_terminate)
+        {
             *ret = 0;
-            return;   //  [Seq-Opt]: can use a goto to reduce this
+            return; //  [Seq-Opt]: can use a goto to reduce this
         }
     }
 
@@ -120,7 +125,8 @@ void single_RM_thread(struct single_thread_RM_args* args){
     return;
 }
 
-int rm_shitty_parallel_pthread(unsigned int test_num, unsigned int k){
+int rm_shitty_parallel_pthread(unsigned int test_num, unsigned int k)
+{
     //  In this case, the k better be the multiple of PTHREAD
     if (test_num < 2)
         return 0;
@@ -142,28 +148,32 @@ int rm_shitty_parallel_pthread(unsigned int test_num, unsigned int k){
     int result[PTHREAD] = {0};
     struct single_thread_RM_args args[PTHREAD];
 
-    for(int i=0; i<PTHREAD; i++){
+    for (int i = 0; i < PTHREAD; i++)
+    {
         args[i].test_num = test_num;
-        args[i].k = k/PTHREAD;
+        args[i].k = k / PTHREAD;
         args[i].d = d;
         args[i].s = s;
         args[i].ret = result + i;
         pthread_create(&(running_threads[i]), NULL, single_RM_thread, &args[i]);
     }
 
-    for(int i=0; i<PTHREAD; i++){
+    for (int i = 0; i < PTHREAD; i++)
+    {
         pthread_join(running_threads[i], NULL);
     }
 
     int ret = 1;
-    for(int i=0; i<PTHREAD; i++){
+    for (int i = 0; i < PTHREAD; i++)
+    {
         ret = ret && result[i];
     }
 
     return ret;
 }
 
-int rabin_miller_v1_parallel(unsigned int test_num, unsigned int k){
+int rabin_miller_v1_parallel(unsigned int test_num, unsigned int k)
+{
     //  First do some input validation
     if (test_num < 2)
         return 0;
@@ -181,42 +191,49 @@ int rabin_miller_v1_parallel(unsigned int test_num, unsigned int k){
         s += 1;
     }
 
-    unsigned int *randoms     = (unsigned int *)(malloc(sizeof(unsigned int) * (k+1)));
-    unsigned int *temp        = (unsigned int *)(malloc(sizeof(unsigned int) * (k+1)));
-    int *terminate   = (int *)(malloc(sizeof(int) * (k+1)));
-    memset(terminate, 0, sizeof(int) * (k+1));
+    unsigned int *randoms = (unsigned int *)(malloc(sizeof(unsigned int) * (k + 1)));
+    unsigned int *temp = (unsigned int *)(malloc(sizeof(unsigned int) * (k + 1)));
+    int *terminate = (int *)(malloc(sizeof(int) * (k + 1)));
+    memset(terminate, 0, sizeof(int) * (k + 1));
 
-    //  Now divide this process into steps and paralize each step individually:
+//  Now divide this process into steps and paralize each step individually:
 
-    //  Generate Random Number, and do the first round calculation
-    #pragma omp parallel for num_threads(PTHREAD)
-    for(unsigned int i=0; i<k; i++){
+//  Generate Random Number, and do the first round calculation
+#pragma omp parallel for num_threads(PTHREAD)
+    for (unsigned int i = 0; i < k; i++)
+    {
         randoms[i] = getRandom(test_num - 1);
         temp[i] = expoMod(randoms[i], d, test_num);
-        if((temp[i] == 1) || (temp[i] == test_num - 1))
+        if ((temp[i] == 1) || (temp[i] == test_num - 1))
             terminate[i] = 1;
     }
 
     //  After first round, we can determine
     int all_done = 1;
-    for(unsigned int i=0; i<k; i++){
+    for (unsigned int i = 0; i < k; i++)
+    {
         all_done = all_done && terminate[i];
     }
-    if(all_done)    return 1;
+    if (all_done)
+        return 1;
 
-    for(unsigned int r = 1; r < s; r++){
-        #pragma omp parallel for num_threads(PTHREAD)
-        for(unsigned int i=0; i<k; i++){
-            if(terminate[i] != 1){
+    for (unsigned int r = 1; r < s; r++)
+    {
+#pragma omp parallel for num_threads(PTHREAD)
+        for (unsigned int i = 0; i < k; i++)
+        {
+            if (terminate[i] != 1)
+            {
                 temp[i] = expoMod(temp[i], 2, test_num);
-                if(temp[i] == test_num - 1)
+                if (temp[i] == test_num - 1)
                     terminate[i] = 1;
             }
         }
 
         //  Sequential examine
-        for(unsigned int i=0; i<k; i++){
-            if(terminate[i] == 0 && temp[i] == 1)    //  Any time we got this answer, it should be composite
+        for (unsigned int i = 0; i < k; i++)
+        {
+            if (terminate[i] == 0 && temp[i] == 1) //  Any time we got this answer, it should be composite
             {
                 //printf("When test %d, terminate here, i: %d, r: %d\n", test_num, i, r);
                 return 0;
@@ -224,11 +241,13 @@ int rabin_miller_v1_parallel(unsigned int test_num, unsigned int k){
         }
 
         all_done = 1;
-        for(unsigned int i=0; i<k; i++){
+        for (unsigned int i = 0; i < k; i++)
+        {
             all_done = all_done && (terminate[i]);
         }
-        if(all_done)    return 1;
+        if (all_done)
+            return 1;
     }
-    
+
     return 0;
 }
--- rm_test.c
+++ rm_test.c
@@ -3,21 +3,57 @@
 
 #define ARRAY_MAX 512
 #define STR_MAX 128
-typedef int (*mr_func) (unsigned int x, unsigned int y);
+typedef int (*mr_func)(unsigned int x, unsigned int y);
 
+#define IMPL_COUNT 5 //  Total functions implemented
+static mr_func mr_func_table[IMPL_COUNT] = {
+    rabin_miller_sf,
+    rabin_miller_shitty,
+    rabin_miller_shitty_parallel,
+    rabin_miller_v1_parallel,
+    rm_shitty_parallel_pthread};
+
+static char mr_func_name_table[ARRAY_MAX][STR_MAX] = {
+    "rabin_miller_sf",
+    "rabin_miller_shitty",
+    "rabin_miller_shitty_parallel",
+    "rabin_miller_v1_parallel",
+    "rm_shitty_parallel_pthread"};
 
 //  Make sure srand can only be called once
 static char rand_initialized = 0;
-static void my_srand(){
+static void my_srand()
+{
     if (rand_initialized == 0)
         return;
+    omp_set_dynamic(0);
+    omp_set_num_threads(PTHREAD);
     srand(time(NULL));
     rand_initialized = 1;
     return;
 }
 
+/*
+    Test for the implementation:
+        print_primality_single_func
+        print_single_input
+        print_prime_in_range
+        print_prime_under_100_for_all
+*/
+void print_primality_single_func(mr_func func, unsigned int test_num, unsigned int test_round)
+{
+    my_srand();
+
+    int result = func(test_num, test_round);
+    int default_result = rabin_miller_sf(test_num, test_round);
+    printf("Checking %d, got %s, default %s\n",
+           test_num, result == 1 ? "prime" : "composite", default_result == 1 ? "prime" : "composite");
+}
 //  Single test, check the primality of a number given by user, by default check 40 rounds
-void test_single_input(){
+void print_single_input()
+{
+    my_srand();
+
     unsigned testing_round = 40;
     unsigned test_num;
     printf("Please enter the number to check:\n");
@@ -25,90 +61,79 @@ void test_single_input(){
     print_primality_single_func(rm_shitty_parallel_pthread, test_num, testing_round);
 }
 
-void print_primality_single_func(mr_func func, unsigned int test_num, unsigned int test_round){
+void print_prime_in_range(int start, int end, int round, int f_index)
+{
     my_srand();
 
-    int result = func(test_num, test_round);
-    int default_result = rabin_miller_sf(test_num, test_round);
-    printf("Checking %d, got %s, default %s\n", 
-        test_num, result==1 ? "prime" : "composite", default_result==1 ? "prime" : "composite");
-}
-
-int testCorrect(){
-    srand(time(NULL));
-    int (*func_ptrs[ARRAY_MAX]) (unsigned int x, unsigned int y) = {
-        rabin_miller_sf,
-        rabin_miller_shitty,
-        rabin_miller_shitty_parallel,
-        rabin_miller_v1_parallel,
-        rm_shitty_parallel_pthread
-    };
-
-    for(int i=3; i<5; i++){
-        int  (*MRfunc) (unsigned int x, unsigned int y) = func_ptrs[i];
-        for(unsigned n=3; n<100; n++){
-            if(MRfunc(n, 40))
-                printf("%d\n", n);
-        }
-        printf("------------------------\n");
+    mr_func func = mr_func_table[f_index];
+    for (unsigned int n = start; n < end; n++)
+    {
+        if (func(n, round))
+            printf("\t\t%d\n", n);
     }
 }
 
-int testBench(){
+void print_prime_under_100_for_all()
+{
     my_srand();
 
-    unsigned int test_start = 1000;
-    unsigned int test_end   = 50000;
-    unsigned int testing_round = 1000;
+    printf("----- Check the correctness for all Implementations -----\n");
 
-    printf("Now benchmarking...\nTesting from %d to %d, each number will be tested for %d round\n",
-    test_start, test_end, testing_round);
-
-    omp_set_dynamic(0);
-    omp_set_num_threads(PTHREAD);
-
-    printf("-- For parallel version, we are using %d threads.\n", PTHREAD);
-
-    //  Array of function pointers
-    int (*func_ptrs[ARRAY_MAX]) (unsigned int x, unsigned int y) = {
-        rabin_miller_sf,
-        //rabin_miller_shitty,
-        rabin_miller_shitty_parallel,
-        //rabin_miller_v1_parallel,
-        rm_shitty_parallel_pthread
-    };
-
-    char func_names[ARRAY_MAX][STR_MAX] = {
-        "rabin_miller_sf",
-        //"rabin_miller_shitty",
-        "rabin_miller_shitty_parallel",
-        //"rabin_miller_v1_parallel",
-        "rm_shitty_parallel_pthread"
-    };
-    int func_len = 3;
-
-
-    for(int i=0; i<func_len; i++){
-        clock_t start = clock(), diff;
-        
-        for(int j=test_start; j<test_end; j++){
-            (*func_ptrs[i])(j, testing_round);
-        }
-
-        diff = clock() - start;
-        int msec = diff * 1000 / CLOCKS_PER_SEC;
-        printf("%35s: Time taken %d seconds %d milliseconds\n", func_names[i], msec/1000, msec%1000);
+    for (int i = 0; i < IMPL_COUNT; i++)
+    {
+        printf("\tNow checking [%s]...\n", mr_func_name_table[i]);
+        print_prime_in_range(3, 100, 40, i);
     }
-
-    return 0;
 }
 
-int main(){
-    //  test_single_input();
-    //  singleTest();
-    //  testCorrect();
-    testCorrect();
-    //  testBench();
-    return 0;
+void testbench_single(unsigned int start, unsigned int end, unsigned int round, int f_index)
+{
+    my_srand();
 
+    FILE *logfd;
+    logfd = fopen("benchmarking.txt", "a+");
+
+    printf("\t********** Now benchmarking **********\n");
+    printf("\t* checking all numbers from %d to %d\n", start, end);
+    printf("\t* each number will be tested for %d round\n", round);
+    printf("\t* For parallel version, we are using %d threads.\n", PTHREAD);
+
+    mr_func func_to_test = mr_func_table[f_index];
+
+    clock_t t_start = clock(), diff;
+    for (int i = start; i < end; i++)
+    {
+        func_to_test(i, round);
+    }
+    diff = clock() - t_start;
+
+    int msec = diff * 1000 / CLOCKS_PER_SEC;
+    fprintf(logfd, "%35s: Time taken %d seconds %d milliseconds\n", mr_func_name_table[f_index], msec / 1000, msec % 1000);
+    printf("%35s: Time taken %d seconds %d milliseconds\n", mr_func_name_table[f_index], msec / 1000, msec % 1000);
+
+    close(logfd);
+}
+
+void testbench_all(unsigned int start, unsigned int end, unsigned int round)
+{
+    for (int i = 0; i < IMPL_COUNT; i++)
+    {
+        testbench_single(start, end, round, i);
+    }
+}
+
+int main()
+{
+    unsigned start  = 4;
+    unsigned end    = 8;
+    unsigned round  = 100;
+
+    for(int i=0; i<30; i++){
+        testbench_single(start, end, round, 0);
+        end = end * 2;
+        start = start * 2;
+    }
+
+    //  testbench_all(500, 5000000, 100);
+    return 0;
 }
