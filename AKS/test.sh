0b96fe004974016c79f4d4c76135f96781390199
Zhengtang <yangzhengtang@utexas.edu>
2021-11-15 16:47:35 -0600

    Temp

--- AKS/test.sh
+++ AKS/test.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
 mkdir ./test-res
 
-#echo "n,logn,isPrime,time" > test-res/serial.csv
-#aks -f selected.txt >> test-res/serial.csv
+echo "n,logn,isPrime,time" > test-res/serial.csv
+aks -f selected.txt >> test-res/serial.csv
 
 echo "n,logn,isPrime,time" > test-res/para-2.csv
-OMP_NUM_THREADS=2 ./test.o >> test-res/para-2.csv
+OMP_NUM_THREADS=2 aks_para -f selected.txt >> test-res/para-2.csv
 
 echo "n,logn,isPrime,time" > test-res/para-4.csv
-OMP_NUM_THREADS=4 ./test.o >> test-res/para-4.csv
+OMP_NUM_THREADS=4 aks_para -f selected.txt >> test-res/para-4.csv
 
 echo "n,logn,isPrime,time" > test-res/para-8.csv
-OMP_NUM_THREADS=8 ./test.o >> test-res/para-8.csv
+OMP_NUM_THREADS=8 aks_para -f selected.txt >> test-res/para-8.csv
 
 echo "n,logn,isPrime,time" > test-res/para-16.csv
-OMP_NUM_THREADS=16 ./test.o >> test-res/para-16.csv
-
-echo "n,logn,isPrime,time" > test-res/para-32.csv
-OMP_NUM_THREADS=32 ./test.o >> test-res/para-32.csv
-
-echo "n,logn,isPrime,time" > test-res/para-64.csv
-OMP_NUM_THREADS=64 ./test.o >> test-res/para-64.csv
+OMP_NUM_THREADS=16 aks_para -f selected.txt >> test-res/para-16.csv
